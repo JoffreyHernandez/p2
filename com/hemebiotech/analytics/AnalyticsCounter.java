c018dc655dd1299c50b5d3439470882e4c64b787
Joffrey HERNANDEZ <joffreyhernandez.mentor.oc@gmail.com>
2022-11-26 11:59:41 +0000

    finish

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,55 +1,30 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Map;
 import java.util.List;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private ISymptomReader reader;
-	private ISymptomWriter writer;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception { 
-	}
+    private final ISymptomReader reader;
+    private final ISymptomWriter writer;
 
-	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
-		this.reader = reader;
-		this.writer = writer;
-	}
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
 
-	public List<String> getSymptoms() { 
-		return this.reader.getSymptoms();
-	}
+    public List<String> getSymptoms() {
+        return this.reader.getSymptoms();
+    }
 
-	public Map<String, Integer> countSymptoms(List<String> symptoms) { 
-	}
+    public Map<String, Integer> sortSymptoms(List<String> symptoms) {
+        Map<String, Integer> result = new TreeMap<>();
+        symptoms.forEach(symptom -> result.put(symptom, result.getOrDefault(symptom, 0) + 1));
 
-	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) { 
-		Map<String, Integer> result = new TreeMap<String, Integer>();
-		
-		try {
-			System.out.printf("Reading symptoms file : %s.%n", filepath);
-			
-			BufferedReader reader = new BufferedReader (new FileReader(filepath));
-			String line = reader.readLine();
-			
-			while (line != null) {
-				result.put(line, result.getOrDefault(line, 0) + 1);
-				line = reader.readLine();
-			}
+        return result;
+    }
 
-			reader.close();
-		} catch (IOException e) {
-			System.err.format("Failed to read the file : %s.%nShutting down application.%n", filepath);
-			System.exit(1);
-		}
-		
-		return result;
-	}
-
-	public void writeSymptoms(Map<String, Integer> symptoms) { 
-
-	} 
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        this.writer.writeSymptoms(symptoms);
+    }
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,9 +1,22 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
-  public void writeSymptoms(Map<String, Integer> symptoms) {
 
+  public void writeSymptoms(Map<String, Integer> symptoms) {
+    try {
+      FileWriter writer = new FileWriter("result.out");
+
+      for (String key : symptoms.keySet()) {
+        writer.write(key + " : " + symptoms.get(key) + "\n");
+      }
+      writer.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
